8ba4beee75550861acf1a79cf614fd5c3373b8f1
moonmouse11 <mmssikg@gmail.com>
2024-03-04 01:03:23 +0500

    Code Refactor

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,20 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Models\Category;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
     use HasFactory;
-    protected $guarded = ['id'];
+    protected $fillable = ['title', 'slug', 'excerpt', 'body'];
     protected $with = ['category', 'author'];
 
-    public function category()
+    public function category(): BelongsTo
     {
         return $this->belongsTo(Category::class);
     }
 
-    public function author()
+    public function author(): BelongsTo
     {
         return $this->belongsTo(User::class, 'user_id');
     }
