5b9b32d77376af856c89ea50ae4261e81b562522
gorshkov-in <gorshkov-in@dom-digital.pro>
2024-11-10 12:52:16 +0500

    Linter routes

--- routes/console.php
+++ routes/console.php
@@ -3,17 +3,9 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
-/*
-|--------------------------------------------------------------------------
-| Console Routes
-|--------------------------------------------------------------------------
-|
-| This file is where you may define all of your Closure based console
-| commands. Each Closure is bound to a command instance allowing a
-| simple approach to interacting with each command's IO methods.
-|
-*/
-
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+Artisan::command(
+    signature: 'inspire',
+    callback: function () {
+        $this->comment(Inspiring::quote());
+    }
+)->purpose(description: 'Display an inspiring quote');
--- routes/web.php
+++ routes/web.php
@@ -7,17 +7,6 @@ use App\Models\Post;
 use App\Models\Category;
 use App\Models\User;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get(uri: '/', action: static function () {
     return view(
         view: 'posts',
